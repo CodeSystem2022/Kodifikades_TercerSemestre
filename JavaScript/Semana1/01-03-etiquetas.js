23e1662c342606c43ee09e2a4929032a28d051c8
Miguel A. Bru Melis <brumelis545@gmail.com>
2023-04-18 10:19:02 -0300

    brumelis Semana1

---
+++ JavaScript/Semana1/01-03-etiquetas.js
@@ -0,0 +1,9 @@
+// Etiqueta Labels
+inicio:
+for(let contando = 0; contando <= 10; contando++){
+    if(contando % 2 !== 0){
+        break inicio; //ir a la siguiente iteracion
+    }
+    console.log(contando);
+}
+console.log("Termina el ciclo");  //Termina el Ciclo
