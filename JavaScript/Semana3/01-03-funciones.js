c02624dfc1611eb70c17df9f0b57d9f960ffb579
Miguel A. Bru Melis <brumelis545@gmail.com>
2023-04-20 11:07:35 -0300

    ejercicios Python y JavaScript rama brumelis

---
+++ JavaScript/Semana3/01-03-funciones.js
@@ -0,0 +1,21 @@
+
+miFuncion(8, 2); //  hosting
+
+// Creamos la funcion
+function miFuncion(a, b){
+//    console.log("Sumamos: "+(a + b));
+    return a + b;  // la funcion regresa un valor. se puede asignar a una variable
+} // aunque no estemos retornando automaticamente el compilador utiliza return
+
+
+// Llamando la funcion
+miFuncion(5, 4);
+
+let resultado = miFuncion(6, 7);
+console.log(resultado); 
+
+
+// Funciones flecha
+const sumarFuncionFlecha = (a,b) => a + b;
+resultado = sumarFuncionFlecha(3,7); // Asignamos el valor a una variable
+console.log(resultado);
