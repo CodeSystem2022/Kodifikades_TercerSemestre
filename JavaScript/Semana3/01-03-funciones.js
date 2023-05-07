9edbe4d35219e47c2c411abf363f52a9e004444f
Lucas <lucasmruiz05@gmail.com>
2023-05-07 18:20:19 -0300

    Add hoisting concept, ruiz lucas miguel, javascript clase 3

--- JavaScript/Semana3/01-03-funciones.js
+++ JavaScript/Semana3/01-03-funciones.js
@@ -1,21 +1,28 @@
-
 miFuncion(8, 2); //  hosting
 
 // Creamos la funcion
-function miFuncion(a, b){
-//    console.log("Sumamos: "+(a + b));
-    return a + b;  // la funcion regresa un valor. se puede asignar a una variable
+function miFuncion(a, b) {
+	//    console.log("Sumamos: "+(a + b));
+	return a + b; // la funcion regresa un valor. se puede asignar a una variable
 } // aunque no estemos retornando automaticamente el compilador utiliza return
-
 
 // Llamando la funcion
 miFuncion(5, 4);
 
 let resultado = miFuncion(6, 7);
-console.log(resultado); 
-
+console.log(resultado);
 
 // Funciones flecha
-const sumarFuncionFlecha = (a,b) => a + b;
-resultado = sumarFuncionFlecha(3,7); // Asignamos el valor a una variable
+const sumarFuncionFlecha = (a, b) => a + b;
+resultado = sumarFuncionFlecha(3, 7); // Asignamos el valor a una variable
 console.log(resultado);
+
+const response = increaseAll(1, 2, 3, 4, 5, 6);
+function increaseAll() {
+	let increment = 0;
+	for (let i = 0; i < arguments.length; i++) {
+		increment = array[i];
+	}
+	return increment;
+}
+console.log(response);
