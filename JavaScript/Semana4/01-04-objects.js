a00b7c172364dfe638a3c6b649c27bcc6346b385
Lucas <lucasmruiz05@gmail.com>
2023-05-07 18:27:11 -0300

    Add different ways to create an object, ruiz lucas miguel, javascript clase 4

--- JavaScript/Semana4/01-04-objects.js
+++ JavaScript/Semana4/01-04-objects.js
@@ -7,5 +7,10 @@ let persona = {
 		return `${this.nombre} + ${this.apellido}`;
 	},
 };
-
 console.log(persona.nombreCompleto());
+
+const persona2 = new Object();
+persona2.nombre = 'Lucas';
+persona2.apellido = 'Ruiz';
+console.log(persona2.nombre);
+console.log(persona2.apellido);
