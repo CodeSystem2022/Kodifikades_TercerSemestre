a57af20e8775af29cd4a4d9b3347036ebc521c06
Lucas <lucasmruiz05@gmail.com>
2023-05-07 18:30:48 -0300

    Add modify and delete property from an object, ruiz lucas miguel, javascript clase 4

--- JavaScript/Semana4/01-04-objects.js
+++ JavaScript/Semana4/01-04-objects.js
@@ -14,3 +14,11 @@ persona2.nombre = 'Lucas';
 persona2.apellido = 'Ruiz';
 console.log(persona2.nombre);
 console.log(persona2.apellido);
+
+for (const key in persona) {
+	console.log(key);
+	console.log(persona[k]);
+}
+persona.apellido = 'Chimuelo';
+delete persona.apellido;
+console.log(persona);
