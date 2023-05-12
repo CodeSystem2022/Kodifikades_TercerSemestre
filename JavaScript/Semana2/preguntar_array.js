17e7cb9efd607f2ab5e0c99a5e44e95a7f451101
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-05-12 11:58:52 -0300

    .

---
+++ JavaScript/Semana2/preguntar_array.js
@@ -0,0 +1,14 @@
+
+
+let autos = []
+
+autos[autos.length] = 'Porsche'
+console.log(autos)
+
+autos[6] = 'Renault'
+console.log(autos)
+
+// como preugntar si es un array
+console.log(Array.isArray(autos)) // devuelve un booleano
+
+console.log(autos instanceof Array )
---
+++ Python/Semana2/Uso_with.py
@@ -0,0 +1,5 @@
+
+
+with open('prueba.txt', 'r', encoding='utf-8') as archivo:
+    print(archivo.read())
+    
