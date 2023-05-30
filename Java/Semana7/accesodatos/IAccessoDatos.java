b66f041b799173a6196ac0c8be33379725e59778
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2023-05-30 11:50:08 -0300

    7.3 Manejo de Interfaces Parte 3

---
+++ Java/Semana7/accesodatos/IAccessoDatos.java
@@ -0,0 +1,13 @@
+package Java.Semana7.accesodatos;
+
+public interface IAccessoDatos {
+    int MAX_REGISTRO = 10; //atributo constante, estático, publico y final
+    
+    //Método insertar: abstracto y sin cuerpo
+    void insertar(); //no tiene {}
+    void listar();
+    void actualizar();
+    void eliminar();
+    //Son todos métodos tipicos al trabajar con base de datos
+    
+}
