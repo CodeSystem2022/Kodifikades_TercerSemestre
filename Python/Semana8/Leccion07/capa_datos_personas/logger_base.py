224caedae37f5cca398779497606ec174d56c0c4
Lucas <lucasmruiz05@gmail.com>
2023-05-30 18:58:41 -0300

    7.2 Manejo de logging: Parte 1, ruiz lucas miguel, python semana 8

---
+++ Python/Semana8/Leccion07/capa_datos_personas/logger_base.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logging.debug('Mensaje a nivel bug')
+logging.info('Mensaje a nivel info')
+logging.warning('Mensaje a nivel warning')
+logging.error('Mensaje a nivel error')
+logging.critical('Mensaje a nivel critical')
