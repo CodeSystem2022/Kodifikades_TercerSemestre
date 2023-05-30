d7e36d1e658c8e9f5631053ccea92f8c527599df
Miguel Angel Bru Melis <brumelis545@gmail.com>
2023-05-30 16:44:43 -0300

    semana 8 JavaScript brumelis

---
+++ JavaScript/Semana8/TSLeccion05/05-03-clases.js
@@ -0,0 +1,52 @@
+class Persona{ 
+    constructor(nombre, apellido){ 
+        this._nombre = nombre; 
+        this._apellido = apellido; 
+    }
+  
+    get nombre(){
+        return this._nombre;
+    }
+    //6.2 Parte Set (Modificamos el atributo)
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    //Tarea Asignada
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    nombreCompleto(){
+        return this._nombre + ' ' + this._apellido;
+    }
+    
+    toString(){ 
+
+        return this.nombreCompleto();
+    }
+}
+
+class Empleado extends Persona{ //Clase hija
+    constructor(nombre, apellido, departamento){
+        super(nombre,apellido);
+        this._departamento = departamento;
+    }
+
+    get departamento(){
+        return this._departamento;
+    }
+
+    set departamento(departamento){
+        this._departamento = this.departamento;
+    }
+
+    // Sobreescritura:
+    nombreCompleto(){
+        //return this.nombre+' '+this._apellido+', departamento: '+ this.departamento;
+        return super.nombreCompleto()+', '+this._departamento; 
+    }
+}
