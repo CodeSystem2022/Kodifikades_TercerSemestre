77f6e40a7820e82bebcf76a7062b769ec979e127
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2023-05-30 11:07:00 -0300

    5.1 Conversión de objetos (Casting)

---
+++ Java/Semana6/ConversionObjetos/test/test/TestConversionObjetos.java
@@ -0,0 +1,35 @@
+package test;
+import domain.*;
+
+public class TestConversionObjetos {
+   public static void main(String[] args) {
+       Empleado empleado;
+       Empleado empleado2;
+       empleado = new Escritor ("Juan", 5000, TipoEscritura.CLASICO);
+       empleado2 = new Escritor ("Juan", 5000, TipoEscritura.CLASICO);
+       //System.out.println("empleado= "+empleado);
+       System.out.println(empleado.obtenerDetalles()); //Si queremos acceder a metodo Escritor
+       //empleado.getTipoEscritura(); No se puede hacer
+        if(empleado == empleado2) {
+            System.out.println("Tienen la misma referencia en memoria.");
+        } else {
+            System.out.println("Tienen distinta referencia en memoria.");
+        }
+
+        if(empleado.equals(empleado2)){
+            System.out.println("Los objetos son iguales.");
+        } else {
+            System.out.println("Los objetos no son iguales.");
+
+        }
+        //Downcasting
+       //((Escritor)empleado).getTipoEscritura(); //Tenemos dos opciones: esta es una
+       Escritor escritor = (Escritor)empleado; //Esta es la segunda opcion
+       escritor.getTipoEscritura();
+   
+       //Upcasting
+       Empleado empleado2 = escritor;
+       System.out.println(empleado2.obtenerDetalles());
+   
+   }
+}
