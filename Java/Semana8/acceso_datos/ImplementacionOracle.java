458d01198eef5339417bb1fbda2f3d03169e8f03
Lucas <lucasmruiz05@gmail.com>
2023-05-30 19:32:44 -0300

    ruiz lucas miguel, java, semana 8

---
+++ Java/Semana8/acceso_datos/ImplementacionOracle.java
@@ -0,0 +1,31 @@
+package Java.Semana8.acceso_datos;
+
+import Java.Semana7.accesodatos.IAccessoDatos;
+
+public class ImplementacionOracle implements IAccessoDatos {
+
+  @Override
+  public void insertar() {
+    // TODO Auto-generated method stub
+    System.out.println("Insertar oracle");
+  }
+  
+  @Override
+  public void listar() {
+    // TODO Auto-generated method stub
+    System.out.println("Listar oracle");
+  }
+  
+  @Override
+  public void actualizar() {
+    // TODO Auto-generated method stub
+    System.out.println("Actualizar oracle");
+  }
+  
+  @Override
+  public void eliminar() {
+    // TODO Auto-generated method stub
+    System.out.println("Eliminar oracle");
+  }
+  
+}
