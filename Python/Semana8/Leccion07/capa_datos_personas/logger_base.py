e605966340999a818bf2014f03661accb3d5ff7f
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-05-31 13:04:46 -0300

    7.3 Manejo de logging: Parte 2

--- Python/Semana8/Leccion07/capa_datos_personas/logger_base.py
+++ Python/Semana8/Leccion07/capa_datos_personas/logger_base.py
@@ -1,8 +1,18 @@
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-logging.debug('Mensaje a nivel bug')
-logging.info('Mensaje a nivel info')
-logging.warning('Mensaje a nivel warning')
-logging.error('Mensaje a nivel error')
-logging.critical('Mensaje a nivel critical')
+#logging.basicConfig(level=logging.DEBUG)
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
+                    datefmt='%I:%M:%S %p',
+                    handlers=[
+                        log.FileHandler('capa_datos.log'),
+                        log.StreamHandler()
+                    ])
+
+if __name__ == '__main__':
+    logging.debug('Mensaje a nivel bug')
+    logging.info('Mensaje a nivel info')
+    logging.warning('Mensaje a nivel warning')
+    logging.error('Mensaje a nivel error')
+    logging.critical('Mensaje a nivel critical')
