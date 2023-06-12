564a02250ad12c08e4220ab07d220c5cb7b945b3
Miguel Angel Bru Melis <brumelis545@gmail.com>
2023-06-12 11:22:49 -0300

    brumelis semana10 java

---
+++ Java/Semana10/CalculadoraUTN.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CalculadoraUTN {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        while (true){ //Ciclo infinito
+
+            System.out.println("******* Aplicacion Calculadora *******");
+            mostrarMenu();
+            
+            try{
+                var operacion = Integer.parseInt(entrada.nextLine());
+                if(operacion >= 1 && operacion <= 4) {
+
+                    ejecutarOperacion(operacion, entrada);
+                }//Fin del if
+
+                else if (operacion == 5){
+                    System.out.println("Hasta pronto...");
+                    break;//Rompe el ciclo y sale
+                }else{
+                    System.out.println("Opción erronea: "+operacion);
+                }
+                //Imprimimos un salto de linea antes de repetir el menu
+                System.out.println();
+            }catch (Exception e){//Fin try,comienza el catch
+                System.out.println("Ocurrio un error: "+e.getMessage());
+                System.out.println();
+            }//Fin catch
+        }//Fin while
+    }//Fin main
