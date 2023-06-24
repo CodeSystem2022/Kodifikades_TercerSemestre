0e8d53bcd98faf988a663ea67626510a89e9e021
Miguel Angel Bru Melis <brumelis545@gmail.com>
2023-06-24 20:20:12 -0300

    brumelis semana11 javascript

---
+++ JavaScript/Semana11/SistemaVentas.js
@@ -0,0 +1,8 @@
+
+// COLOCAR AQUÍ CODIGO 
+
+// 10.3 Prueba de la Clase Producto
+let producto1 = new Producto('Pantalon', 200);
+let producto2 = new Producto('Camisa', 150);
+console.log(producto1.toString());
+console.log(producto2.toString());
