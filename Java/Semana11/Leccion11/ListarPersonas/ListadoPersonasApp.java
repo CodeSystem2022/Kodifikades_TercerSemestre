c3f77f9de5a4f8403721ed48dd4f3903fcfbd524
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2023-06-26 11:52:55 -0300

    0.4 Agregamos el try catch y el método ejecutarOperación()

--- Java/Semana11/Leccion11/ListarPersonas/ListadoPersonasApp.java
+++ Java/Semana11/Leccion11/ListarPersonas/ListadoPersonasApp.java
@@ -18,11 +18,11 @@ public class ListadoPersonasApp {
             }
             System.out.println();
 
-        }
-    }
+        }//Fin del ciclo while
+    }// Fin método main
     
     private static void mostrarMenu(){
-        
+        //Mostramos las opciones
         System.out.print("""
                            *******Listado de Personas*******
                            1. Agregar
@@ -30,7 +30,40 @@ public class ListadoPersonasApp {
                            3. Salir
                            """);
         System.out.print("Digite una de las opciones: ");
+    }//Fin metodo mostrarMenu
+
+
+    private static boolean ejecutarOperacion(Scanner entrada, List<Persona> personas) {
+        var opcion = Integer.parseInt(entrada.nextLine());
+        var salir = false;
+        //Revisamos la opcion digitada a traves de un switch
+        switch (opcion){
+            case 1 ->{
+                System.out.print("Digite el nombre: ");
+                var nombre = entrada.nextLine();
+                System.out.print("Digite el telefono: ");
+                var tel = entrada.nextLine();
+                System.out.print("Digite el correo: ");
+                var email = entrada.nextLine();
+                //creamos el objeto persona
+                var persona = new Persona(nombre, tel, email);
+                //Agregamos la persona a la lista
+                personas.add(persona);
+                System.out.println("La lista tiene: " + personas.size() + " elementos");
+            }//Fin caso 1
+            case 2 -> {//listar a las personas
+                System.out.println("Listado de personas: ");
+                //Mejoras con lambda y el metodo de referencia
+                //personas.forEach((persona) -> System.out.println(persona));
+                personas.forEach(System.out::println);
+            }
+            case 3 ->{
+                System.out.println("Hasta pronto..");
+                salir = true;
+            }
+            default -> System.out.println("Opcion incorrecta: " + opcion);
+        }
+        return salir;
     }
 
-    
-    }}
+}
